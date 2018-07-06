41b6b45738081c2b4f7542370548f63498513a25
mcc <andi.m.mcclure@gmail.com>
2018-07-05 20:03:12 -0400

    README

--- Assets/PluginTest.cs
+++ Assets/PluginTest.cs
@@ -6,7 +6,7 @@ public class PluginTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Plugin test function returns:" + Plugin.UnityPluginCustomTest());
+		Debug.Log("Plugin test function returns: " + Plugin.UnityPluginCustomTest());
 	}
 	
 	// Update is called once per frame
